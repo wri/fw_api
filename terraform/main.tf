a72186a94458890713d1eca44ab640b134c30ca7
owenjs <owen.evans115@gmail.com>
2022-06-16 14:30:48 +0100

    secret name change

--- terraform/main.tf
+++ terraform/main.tf
@@ -124,7 +124,7 @@ module "route53_healthcheck" {
 module "microservice_token_secret" {
   source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.5.1"
   project       = var.project_prefix
-  name          = "fw-service-token"
+  name          = "${var.project_prefix}-service-token"
   secret_string = var.microservice_token_secret_string
 }
 

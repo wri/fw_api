08c7ab70646c11588a7cc8c69f5ceebf51d02994
edward3sc <101396989+edward3sc@users.noreply.github.com>
2022-06-28 15:47:27 +0100

    Update areas.service.js

--- app/src/services/areas.service.js
+++ app/src/services/areas.service.js
@@ -13,7 +13,7 @@ class AreasService {
   static async getUserAreas(userId) {
     logger.info("Get user areas", userId);
     try {
-      let baseURL = config.get("areasAPI.url");
+      let baseURL = config.get("rwAreasAPI.url");
       const response = await axios.default({
         baseURL,
         url: `/area/fw`,
